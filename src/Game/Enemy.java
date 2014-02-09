822890c7d612ab270c7c700c30c8e469d7a0badb
pancisin <pancisin.patrik@hotmail.com>
2014-02-09 23:13:57 +0100

    skuska00

--- src/Game/Enemy.java
+++ src/Game/Enemy.java
@@ -13,7 +13,7 @@ public class Enemy extends AbstractActor {
     public Enemy(World wrld) {
         this.setImage("resources/images/planet0.png");
         this.getImage().setCenterOfRotation(this.getImage().getWidth() / 2, this.getImage().getHeight() / 2);
-        this.setImageSize(80, 80);
+        this.setImageSize(100, 100);
         this.setWorld(wrld);
         
     }
