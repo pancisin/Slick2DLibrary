1e9f80f280df437c1c0d9d0f7106638335bd0bce
johnybiglajs <stevopiskay@gmail.com>
2014-02-09 22:40:59 +0100

    velkost planety aj packagu

--- src/Game/Enemy.java
+++ src/Game/Enemy.java
@@ -13,7 +13,7 @@ public class Enemy extends AbstractActor {
     public Enemy(World wrld) {
         this.setImage("resources/images/planet0.png");
         this.getImage().setCenterOfRotation(this.getImage().getWidth() / 2, this.getImage().getHeight() / 2);
-        this.setImageSize(120, 100);
+        this.setImageSize(300, 300);
         this.setWorld(wrld);
         
     }
